599e11ff18cb62b9ae99dd604e23144a1edc13d3
thiago mendes <thimens@hotmail.com>
2020-02-02 22:38:11 -0800

    Added NameOf class

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,17 +12,47 @@ namespace Test
         {
             DatabaseProviderFactory.RegisterFactory(SqlClientFactory.Instance, "SQL");
             var db = DatabaseProviderFactory.Create(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Projetos\Repos\Thimens.DataMapper\Test\Database.mdf;Integrated Security=True;Connect Timeout=30", "SQL");
+            dynamic nameOf = new NameOf<Client>();
 
-            var query = @"select c.id, c.name, o.Id ""orders.id"", o.deliveryTime ""orders.deliverytime"", p.productId ""orders.products.id"", p.name ""orders.products.name"", p.value ""orders.products.value"" " +
+            Console.WriteLine("Naming product name property:");
+            Console.WriteLine($"ToString: {nameOf.orders.products.name}");
+            Console.WriteLine($"ToSQL: {nameOf.orders.products.name.ToSQL}");
+            Console.WriteLine($"ToDB2: {nameOf.orders.products.name.ToDB2}");
+            Console.WriteLine("");
+
+
+            var query = @$"select c.id, c.name, o.Id {nameOf.Orders.Id.ToSQL}, o.deliveryTime {nameOf.Orders.DeliveryTime.ToSQL}, p.productId {nameOf.Orders.Products.Id.ToSQL}, p.name {nameOf.orders.Products.Name.ToSQL}, p.value {nameOf.orders.Products.Value.ToSQL} " +
                 "from client c inner join [order] o " +
                         "on c.id = o.clientId " + 
                     "inner join order_product p " +
                         "on o.id = p.orderId " +
                 "where c.id = 1";
 
-            var client = db.Get<Client>(CommandType.Text, query, null, "orders.id", "orders.products.id");
+            var client = db.Get<Client>(CommandType.Text, query, null, (string)nameOf.orders.id, (string)nameOf.orders.products.id);
 
-            query = @"select c.id, c.name, o.Id ""ordersid.id"" " +
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"ID: {client.ID}");
+            Console.WriteLine($"Name: {client.Name}");
+            Console.WriteLine($"Orders:");
+
+            foreach (var order in client.Orders)
+            {
+                Console.WriteLine($"  ID: {order.ID}");
+                Console.WriteLine($"  Delivery: {order.DeliveryTime}");
+                Console.WriteLine($"  Products:");
+
+                foreach (var product in order.Products)
+                {
+                    Console.WriteLine($"    ID: {product.ID}");
+                    Console.WriteLine($"    Name: {product.Name}");
+                    Console.WriteLine($"    Value: {product.Value}");
+                    Console.WriteLine("");
+                }
+            }
+
+            // example no2
+
+            query = @$"select c.id, c.name, o.Id [{nameOf.ordersid}.id] " +
                 "from client c inner join [order] o " +
                         "on c.id = o.clientId " +
                     "inner join order_product p " +
@@ -31,32 +61,92 @@ namespace Test
 
             client = db.Get<Client>(CommandType.Text, query, null);
 
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"ID: {client.ID}");
+            Console.WriteLine($"Name: {client.Name}");
+            Console.WriteLine($"Orders:");
+
+            foreach (var orderID in client.OrdersID)
+                Console.WriteLine($"  ID: {orderID}");
+
+            // exmaple no3
+
             query = @"select count(*) from client" ;
 
             var count = db.Get<int>(CommandType.Text, query, null);
 
-            query = @"select c.id, c.name, o.Id ""ordersid.id"" " +
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Client count: {count}");
+
+            // example no4
+
+            query = $@"select c.id, c.name, o.Id [{nameOf.ordersid}.id] " +
                 "from client c inner join [order] o " +
                         "on c.id = o.clientId " +
                     "inner join order_product p " +
                         "on o.id = p.orderId " +
                 "where c.id in (1, 2)";
 
-            var clients = db.Get<IEnumerable<Client>>(CommandType.Text, query, null, "id", "ordersid.id");
+            var clients = db.Get<IEnumerable<Client>>(CommandType.Text, query, null, (string)nameOf.id, $"{nameOf.ordersid}.id");
 
-            query = @"select c.id, c.name, o.Id ""orders.id"", o.deliveryTime ""orders.deliverytime"", p.productId ""orders.products.id"", p.name ""orders.products.name"", p.value ""orders.products.value"" " +
+            Console.WriteLine("---------------------------");
+
+            foreach (var cl in clients)
+            {
+                Console.WriteLine($"ID: {cl.ID}");
+                Console.WriteLine($"Name: {cl.Name}");
+                Console.WriteLine($"Orders:");
+
+                foreach (var orderID in cl.OrdersID)
+                    Console.WriteLine($"  ID: {orderID}");
+            }
+            // example no5
+
+            query = $@"select c.id, c.name, o.Id [{nameOf.orders.id}], o.deliveryTime [{nameOf.orders.deliverytime}], p.productId [{nameOf.orders.products.id}], p.name [{nameOf.orders.products.name}], p.value [{nameOf.orders.products.value}] " +
                 "from client c inner join [order] o " +
                         "on c.id = o.clientId " +
                     "inner join order_product p " +
                         "on o.id = p.orderId " +
                 "where c.id in (1, 2)";
 
-            clients = db.Get<IEnumerable<Client>>(CommandType.Text, query, null, "id", "orders.id", "orders.products.id");
+            clients = db.Get<IEnumerable<Client>>(CommandType.Text, query, null, (string)nameOf.id, (string)nameOf.orders.id, (string)nameOf.orders.products.id);
+
+            Console.WriteLine("---------------------------");
+
+            foreach (var cl in clients)
+            {
+                Console.WriteLine($"ID: {cl.ID}");
+                Console.WriteLine($"Name: {cl.Name}");
+                Console.WriteLine($"Orders:");
+
+                foreach (var order in cl.Orders)
+                {
+                    Console.WriteLine($"  ID: {order.ID}");
+                    Console.WriteLine($"  Delivery: {order.DeliveryTime}");
+                    Console.WriteLine($"  Products:");
+
+                    foreach (var product in order.Products)
+                    {
+                        Console.WriteLine($"    ID: {product.ID}");
+                        Console.WriteLine($"    Name: {product.Name}");
+                        Console.WriteLine($"    Value: {product.Value}");
+                        Console.WriteLine("");
+                    }
+                }
+            }
+
+            // example no6
 
             query = @"select p.productid from order_product p";
 
             var prods = db.Get<IEnumerable<int>>(CommandType.Text, query, null);
 
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Products: ");
+            foreach (var productID in prods)
+                Console.WriteLine($"  {productID}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Thimens.DataMapper/NameOf.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+using System.Text;
+
+namespace Thimens.DataMapper
+{
+    public class NameOf<T> : DynamicObject
+    {
+        internal string _name;
+        private readonly string _prefix = string.Empty;
+        private readonly string _suffix = string.Empty;
+
+        public NameOf() { }
+
+        public NameOf(string prefix, string suffix)
+        {
+            _prefix = prefix;
+            _suffix = suffix;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="affix"></param>
+        public NameOf(NameOfAffix affix)
+        {
+            switch (affix)
+            {
+                case NameOfAffix.SQL:
+                    _prefix = "[";
+                    _suffix = "]";
+                    break;
+
+                case NameOfAffix.DB2:
+                    _prefix = "\"";
+                    _suffix = "\"";
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(affix), affix, null);
+            }
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = GetProperty(binder.Name);
+            return result != null;
+        }
+
+        public override string ToString() => $"{_prefix}{_name}{_suffix}";
+        public string ToSQL => $"[{_name}]";
+        public string ToDB2 => $@"""{_name}""";
+
+        public static implicit operator string(NameOf<T> p) => p.ToString();
+
+        private dynamic GetProperty(string propertyName)
+        {
+            //get property
+            var property = typeof(T).GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.IgnoreCase)
+                ?? throw new MissingMemberException($@"Member ""{propertyName}"" of ""{typeof(T).Name}"" was not found");
+
+            var propertyType = property.PropertyType;
+
+            // check if property is list and get inner type
+            if (IsListType(propertyType))
+                propertyType = property.PropertyType.IsArray ? propertyType.GetElementType() : propertyType.GetGenericArguments()[0];
+
+            var type = typeof(NameOf<>).MakeGenericType(propertyType);
+            var name = $"{_name}{(string.IsNullOrEmpty(_name) ? string.Empty : ".")}{property.Name}";
+            var result = (dynamic)Activator.CreateInstance(type, _prefix, _suffix);
+            result._name = name;
+
+            return result;
+        }
+
+        private static bool IsListType(Type type)
+        {
+            return typeof(IEnumerable).IsAssignableFrom(type) && type != typeof(string);
+        }
+    }
+
+    public enum NameOfAffix
+    {
+        SQL,
+        DB2
+    }
+}
